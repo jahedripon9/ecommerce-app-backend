47420aac700360e4babfe3a41178c22347116f85
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2024-11-13 10:15:52 +0600

    update product Controller

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import 'dotenv/config.js'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 // App Config
 const app = express()
@@ -19,6 +20,7 @@ app.use(cors())
 // Api endpoint
 
 app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
 
 app.get('/', (req, res) => {
     res.send("API Working")
